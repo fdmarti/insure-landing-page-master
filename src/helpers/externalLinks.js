a4564eb2ce6aea4fd62b6ad2479995ce3b65edab
f-d.martinez <f-d.martinez@viseven.com>
2023-09-04 15:10:57 -0300

    first commit

---
+++ src/helpers/externalLinks.js
@@ -0,0 +1,67 @@
+export const FirstLinks = [
+    {
+        title: 'How we work',
+        link: '#',
+    },
+    {
+        title: 'Why insure?',
+        link: '#',
+    },
+    {
+        title: 'view plans',
+        link: '#',
+    },
+    {
+        title: 'Reviews',
+        link: '#',
+    },
+];
+
+export const SecondLinks = [
+    {
+        title: 'FAQ',
+        link: '#',
+    },
+    {
+        title: 'Terms of use',
+        link: '#',
+    },
+    {
+        title: 'Privacy policy',
+        link: '#',
+    },
+    {
+        title: 'Cookies',
+        link: '#',
+    },
+];
+
+export const ThirdLinks = [
+    {
+        title: 'Sales',
+        link: '#',
+    },
+    {
+        title: 'Support',
+        link: '#',
+    },
+    {
+        title: 'Live chat',
+        link: '#',
+    }
+];
+
+export const FourthLinks = [
+    {
+        title: 'Careers',
+        link: '#',
+    },
+    {
+        title: 'Press',
+        link: '#',
+    },
+    {
+        title: 'Licenses',
+        link: '#',
+    }
+];
